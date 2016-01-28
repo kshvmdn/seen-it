7669de047ef5a2187f77146d4fb2d29242b2660a
Kashav Madan <kshvmdn@gmail.com>
2016-01-27 20:20:00 -0500

    Add reddit service, main ctrl

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -21,3 +21,33 @@ var app = angular
       });
     $locationProvider.html5Mode(true);
   });
+app.factory('reddit', ['$http', '$q', function($http, $q) {
+  var getJSON = function() {
+    var deferred = $q.defer();
+    $http.get('https://www.reddit.com/r/videos.json?limit=100')
+      .success(function(data) {
+        var posts = data.data.children;
+        posts.forEach(function(post, i) {
+          if (post.data.is_self) {
+            posts.splice(i, 1);
+          }
+        });
+        deferred.resolve(posts);
+      })
+      .error(function(err) {
+        deferred.reject(err);
+      });
+    return deferred.promise;
+  };
+  return {
+    getData: getJSON
+  };
+}]);
+
+app.controller('MainCtrl', ['$scope', 'reddit', function($scope, reddit){
+  $scope.data = '';
+  reddit.getData().then(function(data) {
+    console.log(data);
+    $scope.data = data;
+  });
+}]);
