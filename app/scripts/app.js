fc9f4e9e606cbe79dbfebfd5b41415a31146e541
Kashav Madan <kshvmdn@gmail.com>
2016-01-27 20:19:25 -0500

    Bootstrap project

---
+++ app/scripts/app.js
@@ -0,0 +1,23 @@
+'use strict';
+
+var app = angular
+  .module('seenitApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider, $locationProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+    $locationProvider.html5Mode(true);
+  });
