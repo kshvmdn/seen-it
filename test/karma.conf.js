7f01751e4ff89829a337a4bdee1fee476434d44a
Kashav Madan <kshvmdn@gmail.com>
2016-01-28 00:01:39 -0500

    Update tests

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -29,9 +29,7 @@ module.exports = function(config) {
       'bower_components/angular-moment/angular-moment.js',
       'bower_components/angular-mocks/angular-mocks.js',
       // endbower
-      "app/scripts/**/*.js",
-      "test/mock/**/*.js",
-      "test/spec/**/*.js"
+      "app/scripts/**/*.js"
     ],
 
     // list of files / patterns to exclude
