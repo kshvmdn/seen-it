fc96c6b6610b0176c496e9b233c1cae0c7358098
Kashav Madan <kshvmdn@gmail.com>
2016-06-13 00:50:04 -0400

    Get logo from redditstatic

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -7,7 +7,7 @@ export default class Header extends React.Component {
   render() {
     return (
       <div className={styles.topbar}>
-        <a href='http://reddit.com' title="Reddit"><img className={styles.snoo} src='assets/images/snoo.png' /></a>
+        <a href='http://reddit.com' title="Reddit"><img className={styles.snoo} src='https://www.redditstatic.com/about/assets/reddit-alien.png' /></a>
         <Link className={styles.logo} to='/'><h1>SEEN IT</h1></Link>
         <section>for <a href='http://reddit.com' title="Reddit">reddit</a>.</section>
       </div>
