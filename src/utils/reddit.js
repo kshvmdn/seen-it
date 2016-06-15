76e2b2cdd6b4e76011841c1ceb0e31713be90faf
Kashav Madan <kshvmdn@gmail.com>
2016-06-15 19:02:48 -0400

    Clean

--- src/utils/reddit.js
+++ src/utils/reddit.js
@@ -10,25 +10,3 @@ export function getTopPosts(subreddit) {
   let api = `${BASE}/r/${subreddit}.json?limit=100`
   return axios.get(api)
 }
-
-// export function getTopPosts(subreddit) {
-//   return new Promise((resolve, reject) => {
-//     let base_api = `${BASE}/r/${subreddit}.json?limit=100`
-//     let api = base_api
-
-//     let videos = []
-//     for (var i = 0; i < 5; i++) {
-//       console.log(i)
-//       axios.get(api)
-//         .then((response) => {
-//           Array.prototype.push.apply(videos, response.data.data.children)
-//           api = `${base_api}&after=${response.data.data.after}`
-//           if (i === 4) {
-//             console.log(videos.length)
-//             resolve(videos)
-//           }
-//         })
-//         .catch(err => reject(err))
-//     }
-//   })
-// }
